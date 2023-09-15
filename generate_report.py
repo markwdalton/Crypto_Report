abcf920e0a63cbe7904ed6b2b419f002f9562349
markwdalton <markwdalton@gmail.com>
2023-09-14 23:47:52 -0500

    Update json output from coingecko

--- generate_report.py
+++ generate_report.py
@@ -38,7 +38,6 @@ if __name__ == '__main__':
     dt_current = datetime.datetime.now().strftime('%m%d%y-%H%M%S')
 
     report_file = f'{report_directory}/gecko_report-{dt_current}.csv'
-
     report_fields = [
         'name',
         'symbol',
@@ -55,6 +54,7 @@ if __name__ == '__main__':
         'price_change_percentage_24h_in_currency',
         'price_change_percentage_7d_in_currency',
         'price_change_percentage_30d_in_currency',
+        'fully_diluted_valuation',
         'ath',
         'ath_change_percentage',
         'ath_date',
@@ -66,8 +66,64 @@ if __name__ == '__main__':
         'total_supply',
         'total_volume',
         'image',
+        'max_supply',
         'last_updated'
     ]
+#
+#    report_fields = [
+#        'name',
+#        'symbol',
+#        'id',
+#        'market_cap_rank',
+#        'market_cap',
+#        'market_cap_change_24h',
+#        'market_cap_change_percentage_24h',
+#        'current_price',
+#        'high_24h',
+#        'low_24h',
+#        'price_change_24h',
+#        'price_change_percentage_24h',
+#        'ath',
+#        'ath_change_percentage',
+#        'ath_date',
+#        'atl',
+#        'atl_change_percentage',
+#        'atl_date',
+#        'roi',
+#        'circulating_supply',
+#        'total_supply',
+#        'total_volume',
+#        'image',
+#        'max_supply',
+#        'last_updated'
+#    ]
+#
+#    "id": "dsun-token",
+#    "symbol": "dsun",
+#    "name": "Dsun Token",
+#    "image": "https://assets.coingecko.com/coins/images/29973/large/500-200.png?1682395330",
+#    "current_price": 3.271e-9,
+#    "market_cap": 0,
+#    "market_cap_rank": null,
+#    "fully_diluted_valuation": 3271153,
+#    "total_volume": 440.27,
+#    "high_24h": null,
+#    "low_24h": null,
+#    "price_change_24h": null,
+#    "price_change_percentage_24h": null,
+#    "market_cap_change_24h": null,
+#    "market_cap_change_percentage_24h": null,
+#    "circulating_supply": 0,
+#    "total_supply": 1000000000000000,
+#    "max_supply": 1000000000000000,
+#    "ath": 1.2677e-8,
+#    "ath_change_percentage": -74.19527,
+#    "ath_date": "2023-04-26T12:39:11.199Z",
+#    "atl": 3.215e-9,
+#    "atl_change_percentage": 1.73359,
+#    "atl_date": "2023-05-17T14:14:23.166Z",
+#    "roi": null,
+#    "last_updated": "2023-05-18T14:00:16.888Z"
 
     logger.info('Writing data to csv file.')
     with open(report_file, 'w', newline='') as csv_file:
